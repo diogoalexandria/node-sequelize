9d025d12c8f193871fa67067cbb03633e827be3a
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-01-30 18:03:14 -0300

    Criação de configuração e tabela no postgres

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'docker',
+    password: 'docker',
+    database: 'postgres-estudo',
+    define: {
+        timestamp: true,
+        underscored: true,
+    },
+};
